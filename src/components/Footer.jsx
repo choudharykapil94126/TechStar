5ac5bcd1ca11278d0b1793ecbf7d87b6b3a39d08
Kapil <choudharykapil94126@gmail.com>
2023-05-16 16:20:53 +0530

    commit file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Footer() {
+  return (
+   <footer>
+        <div>
+        <h1>
+            TechyStar
+        </h1>
+        <p>@all rights reserved</p>
+    </div>
+    <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://github.com/choudharykapil94126" target={"blank"} > GitHub </a>
+        </div>
+    </div>
+   </footer>
+  )
+}
+
+export default Footer;
